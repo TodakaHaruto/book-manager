e072dac5c4c57ae2bb4418ea9e561b9b1a5cf32c
Todaka Haruto <todaharu0422@gmail.com>
2026-05-24 15:12:18 +0900

    関数のリファクタリング

--- src/app/error.js
+++ src/app/error.js
@@ -1,7 +1,15 @@
 'use client';
 
-const errorPage = () => {
-    return (
-        <p className="text-center">エラーが発生しました</p>
-    )
+export default function Error({ error, reset }) {
+  return (
+    <div className="pt-32 text-center text-white">
+      <p>エラーが発生しました</p>
+      <button
+        onClick={() => reset()}
+        className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
+      >
+        再試行
+      </button>
+    </div>
+  );
 }

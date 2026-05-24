24b32b04aec453e9d9eaebff13a01788dc45204a
Todaka Haruto <todaharu0422@gmail.com>
2026-05-24 14:54:03 +0900

    設定の変更

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
   images: {
     remotePatterns: [
       {
-        hostname: "books.google.com",
+        hostname: 'books.google.com',
       },
     ],
   },

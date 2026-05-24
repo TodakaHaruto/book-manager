4573040fb825b27d25579d91aba7a7e674d7bd4b
Todaka Haruto <todaharu0422@gmail.com>
2026-05-24 15:18:01 +0900

    ページの変更

--- src/app/search/[[...keyword]]/page.js
+++ src/app/search/[[...keyword]]/page.js
@@ -2,26 +2,30 @@ import React from "react";
 import LinkedBookList from "../../../components/LinkedBookList";
 import { getBooksByKeyword } from "../../../lib/getter";
 
-
-export default async function searchResult({params: {keyword = undefined} }) {
-    if(keyword == undefined) {
-        return (
-            <p className="text-center">キーワードを入力して検索してください</p>
-        );
-    }
-
-    const books = await getBooksByKeyword(keyword);
-    if(books === null) {
-        return (
-            <p className="text-center">お探しの書籍は見つかりませんでした</p>
-        )
-    }
-    
+export default async function searchResult({ params: { keyword = undefined } }) {
+  if (keyword == undefined) {
     return (
-        <>
-            {books.map((b,i) => (
-                <LinkedBookList book={b} index={i + 1} key={b.id} link={'edit'}/>
-            ))} 
-        </>
-    )
+      <p className="text-center text-white">
+        キーワードを入力して検索してください
+      </p>
+    );
+  }
+
+  const books = await getBooksByKeyword(keyword);
+
+  if (!books || books.length === 0) {
+    return (
+      <p className="text-center text-white">
+        お探しの書籍は見つかりませんでした
+      </p>
+    );
+  }
+
+  return (
+    <>
+      {books.map((b, i) => (
+        <LinkedBookList book={b} index={i + 1} key={b.id} link={"edit"} />
+      ))}
+    </>
+  );
 }
